5adecc28b160e4e392080fa54ec18a1acf90eab5
DGuskov <denis180391@yandex.ru>
2023-02-04 18:32:10 +0300

    Added Task08H

---
+++ Task08H/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе
+// показывает все четные числа от 1 до N.
+// 5 -> 2, 4
+//8 -> 2, 4, 6, 8
+int StartNum = 1;
+Console.WriteLine("Введите натуральное число:");
+int Num = Convert.ToInt32(Console.ReadLine());
+if (Num > StartNum)
+{
+    for (int count = StartNum; count <= Num; count++)
+    {
+        if (count % 2 == 0)
+        {
+            Console.Write(count + " ");
+        }
+        
+    }
+}
+else
+{
+    Console.WriteLine("число должно быть больше одного");
+}
